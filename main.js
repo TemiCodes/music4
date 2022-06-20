c687489e44d7f6ab231400606fc110143f055ba2
TemiCodes <86966766+TemiCodes@users.noreply.github.com>
2022-06-19 21:52:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song="";
+leftwristx=0;
+rightwristx=0;
+leftwristy=0;
+rightwristy=0;
+
+function setup() {
+    canvas=createCanvas(500,500);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+    posenet=ml5.poseNet(video, modelloaded);
+    posenet.on("pose", gotposes);
+    
+
+
+}
+function draw() {
+    image(video,0,0,500,500);
+}
+
+function preload() {
+    song=loadSound("something.mp3");
+}
+function Play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelloaded() { 
+    console.log("modelloaded"); 
+}
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        console.log(leftwristx)
+        console.log(leftwristy)
+        console.log(rightwristx)
+        console.log(rightwristy)
+    }
+}
